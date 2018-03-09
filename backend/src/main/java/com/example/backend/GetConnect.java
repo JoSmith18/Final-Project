bb0fdd16d881c84cf7a0a9c40b2be8c0ccdfad53
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2018-03-09 14:25:12 -0600

    Created my files to do my SQL statements and to help me keep track of all their data in the table

---
+++ backend/src/main/java/com/example/backend/GetConnect.java
@@ -0,0 +1,11 @@
+package com.example.backend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GetConnect {
+    public static Connection get() throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql:bccaGradebook", "basecamp", "pgpass");
+    }
+}
