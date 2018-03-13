edc12edc412504496158e7d9cbf067b43edb04a1
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2018-03-13 10:27:28 -0500

    I add a statement to get all members out the member table

--- backend/src/main/java/com/example/backend/memberRepository.java
+++ backend/src/main/java/com/example/backend/memberRepository.java
@@ -1,5 +1,33 @@
 package com.example.backend;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class memberRepository {
 
+    public static ArrayList<member> allMembers(){
+        try {
+            Connection conn = GetConnect.get();
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "SELECT * FROM members");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            ArrayList<member> allMember = new ArrayList<member>();
+            while (resultSet.next()){
+                allMember.add(new
+                        member(resultSet.getInt("id"),
+                        resultSet.getString("memberName"),
+                        resultSet.getInt("age"),
+                        resultSet.getInt("matchingPoints"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password")));
+            }
+            return allMember;
+        }
+        catch (SQLException e){
+            return null;
+        }
+    }
 }
