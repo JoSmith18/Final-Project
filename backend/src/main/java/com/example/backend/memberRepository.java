0e4640bdb592f2d0c8008f00f7d93431b1fb20d5
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2018-03-13 19:19:37 -0500

    Just made a statement to insert into the members table

--- backend/src/main/java/com/example/backend/memberRepository.java
+++ backend/src/main/java/com/example/backend/memberRepository.java
@@ -30,4 +30,27 @@ public class memberRepository {
             return null;
         }
     }
+
+    public static member insertMember(String memberName,Integer age, String email, String password){
+        try {
+            Connection conn = GetConnect.get();
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "INSERT INTO members (" +
+                            "memberName,age,email,password) " +
+                            "VALUES (?,?,?,?)");
+            preparedStatement.setString(1,memberName);
+            preparedStatement.setInt(2,age);
+            preparedStatement.setString(3,email);
+            preparedStatement.setString(4,password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return new member(resultSet.getInt("id"),resultSet.getString("memberName"),
+                    resultSet.getInt("age"), resultSet.getInt("matchingPoints"),
+                    resultSet.getString("email"),resultSet.getString("password"));
+
+        }
+        catch (SQLException e){
+            return null;
+        }
+    }
+    
 }
